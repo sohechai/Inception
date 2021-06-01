cee1939f597db2e88e696c488697b6c7d07ff468
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro.local>
2021-06-01 23:04:00 +0200

    redirection localhost ok, plus de hacky patch

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -6,21 +6,22 @@
 #    By: sohechai <sohechai@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/05/24 19:46:44 by sohechai          #+#    #+#              #
-#    Updated: 2021/05/31 18:29:08 by sohechai         ###   ########lyon.fr    #
+#    Updated: 2021/06/01 23:02:11 by sohechai         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
+# wordpress installation + setup
 cd /var/www/html
 mv /tmp/wp-config.php ./
-
-# wordpress installation + setup
 wp core download --allow-root
 wp core config --dbname=wordpress --dbuser=wordpress_user --dbpass=wordpress_password --dbhost=mariadb --dbprefix=wp_ --allow-root
-wp core install --url="localhost" --title="INCEPTION" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
+wp core install --url="sohechai.42.fr" --title="INCEPTION" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
+wp user create $SUB_USER $SUB_EMAIL --user_pass=$SUB_PASSWORD --role=subscriber --allow-root
 
 #droit
 chown www-data:www-data /var/www
 chmod 744 /var/www
 
+#run php
 mkdir -p /run/php
-php-fpm7.3 --nodaemonize
+php-fpm7.3 -F
