529d3f024224597eaea678bf5f25dddd4685d4e8
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro.local>
2021-06-03 21:38:47 +0200

    site ok bonus ok

--- srcs/requirements/website/script.sh
+++ srcs/requirements/website/script.sh
@@ -6,8 +6,8 @@
 #    By: sohechai <sohechai@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/03 20:56:41 by sohechai          #+#    #+#              #
-#    Updated: 2021/06/03 20:57:32 by sohechai         ###   ########lyon.fr    #
+#    Updated: 2021/06/03 21:34:49 by sohechai         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
-cp -rf ./react-inception/* /var/www/html/
+cp -rf ./site/* /var/www/html/
