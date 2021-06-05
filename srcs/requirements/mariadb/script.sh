bd9edd1ba4562cfe83973530ec892a525b053d3b
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro.local>
2021-06-06 00:01:43 +0200

    volumes ok, vm ok

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -6,16 +6,17 @@
 #    By: sohechai <sohechai@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/02 15:08:40 by sohechai          #+#    #+#              #
-#    Updated: 2021/06/02 19:28:20 by sohechai         ###   ########lyon.fr    #
+#    Updated: 2021/06/05 23:59:43 by sohechai         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 if [ ! -d "/tmp/ok" ]; then
-	service mysql start
-	echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root -ppass
-	echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" | mysql -u root -ppass
-	echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" | mysql -u root -ppass
-	echo "FLUSH PRIVILEGES;" | mysql -u root -ppass
+	mysql_install_db
+    service mysql start
+    mysql -e "CREATE USER '$DB_USER'@'localhost' identified by '$DB_PASS';" &&\
+	mysql -e "CREATE DATABASE wordpress;" &&\
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" &&\
+	mysql -e "FLUSH PRIVILEGES;"
 	mkdir /tmp/ok
 
 fi
